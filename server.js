78d3680609bfd4e874e1bbd19476a4b372b59d9d
Chad Butler <you@example.com>
2018-09-18 20:55:41 -0400

    heroku stuff

--- server.js
+++ server.js
@@ -25,11 +25,11 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI;
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 //mongoose.connect(MONGODB_URI);
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI || "mongodb://localhost/mongoHeadlines", { useMongoClient: true });
 
 // Routes
 // A GET route for scraping the echoJS website
